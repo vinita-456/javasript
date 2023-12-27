ebf1e21dfb23cb0ce6392b0821165302771824d7
vinita-456 <vinitakushwaha2003@gmail.com>
2023-12-27 10:22:34 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let name="vinita"
+let favfood="fastfood"
+let favplace="nainital"
+let favsubject="english"
+console.log(`my name is ${name} and my favfood is  ${favfood} I love travelling  to my favplace ${favplace},favsubject ${favsubject}`);
